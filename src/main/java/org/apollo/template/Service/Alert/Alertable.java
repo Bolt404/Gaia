5e765d6b0a20c6d33da4a4627f62165515062737
Elias Therkildsen <73726902+eliastherkildsen@users.noreply.github.com>
2024-04-23 13:23:14 +0200

    created the gui for autocamper :D

--- src/main/java/org/apollo/template/Service/Alert/Alertable.java
+++ src/main/java/org/apollo/template/Service/Alert/Alertable.java
@@ -2,7 +2,7 @@ package org.apollo.template.Service.Alert;
 
 public interface Alertable <T>{
 
-    void getAlertArea();
+    T getAlertArea();
     void clearAlertArea();
     void addAlert(AlertComp alertComp);
     void removeAlert(AlertComp alertComp);
