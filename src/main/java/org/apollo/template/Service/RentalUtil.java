c48c8de43f77035d2ea0191e461edae884b80de5
Elias Therkildsen <73726902+eliastherkildsen@users.noreply.github.com>
2024-04-24 15:22:55 +0200

    fixed shits

--- src/main/java/org/apollo/template/Service/RentalUtil.java
+++ src/main/java/org/apollo/template/Service/RentalUtil.java
@@ -22,14 +22,50 @@ public class RentalUtil {
         List<Autocamper> autocampers = new ArrayList<>();
         int cnt = 1;
         try{
-            PreparedStatement ps = con.prepareStatement("SELECT a.fld_chassisNo FROM tbl_autocamper a WHERE NOT EXISTS (SELECT 1 FROM tbl_rental r WHERE r.fld_chassisNo = a.fld_chassisNo AND ((r.fld_startDate <= ? AND r.fld_endDate >= ?)))");
+            PreparedStatement ps = con.prepareStatement("USE DB_Gaia;" +
+                    " SELECT a.fld_chassisNo" +
+                    ",a.fld_registrationNo" +
+                    " ,a.fld_kmCount" +
+                    " ,a.fld_noOfRental" +
+                    ",a.fld_mainSeasonPrice" +
+                            " ,a.fld_lowSeasonPrice" +
+                    " ,a.fld_weight" +
+                    " ,a.fld_length" +
+                            " ,a.fld_width" +
+                    " ,a.fld_height" +
+                    ",a.fld_purchaseDate" +
+                    " ,a.fld_noOfBeds" +
+                    ",a.fld_noOfToilets" +
+                    ",a.fld_noOfSeatbelts" +
+                    ",a.fld_brand" +
+                    ",a.fld_comment" +
+      ",a.fld_type FROM tbl_autocamper a WHERE NOT EXISTS (SELECT 1 FROM tbl_rental r WHERE r.fld_chassisNo = a.fld_chassisNo AND ((r.fld_startDate <= ? AND r.fld_endDate >= ?)))");
+
             ps.setDate(1, startDate);
             ps.setDate(2, endDate);
 
             ResultSet rs = ps.executeQuery();
             while(rs.next()){
-                autocampers.add(dao.read(rs.getString(cnt)));
-               // cnt++;
+                autocampers.add(new Autocamper(
+
+                        rs.getString(1),
+                        rs.getString(2),
+                        rs.getString(15),
+                        rs.getString(16),
+                        rs.getString(17),
+                        rs.getInt(3),
+                        rs.getInt(4),
+                        rs.getInt(7),
+                        rs.getInt(8),
+                        rs.getInt(9),
+                        rs.getInt(12),
+                        rs.getInt(13),
+                        rs.getInt(14),
+                        rs.getInt(15),
+                        rs.getFloat(5),
+                        rs.getFloat(6)
+                ));
+
             }
 
         } catch (SQLException e){
